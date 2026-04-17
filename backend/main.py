2e617f3d2d65687570d0ba6add54ca921d960d0f
drmbx <jankoooy12@gmail.com>
2026-04-17 13:36:30 +0300

    Add updated backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print("Client connected")
+
+    try:
+        while True:
+            data = await websocket.receive_bytes()
+
+            print(f"Received audio chunk: {len(data)} bytes")
+
+            # echo назад
+            await websocket.send_bytes(data)
+
+    except WebSocketDisconnect:
+        print("Client disconnected")
